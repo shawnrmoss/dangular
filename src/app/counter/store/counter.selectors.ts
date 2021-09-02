4ff97ccdc4bc1265277fbe911106b206f1e017e7
shawn <shawnrmoss@gmail.com>
2021-09-01 19:32:08 -0600

    Delete old store

--- src/app/counter/store/counter.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as fromCounter from './counter.reducer';
-
-export const selectCounterState = createFeatureSelector<fromCounter.State>(
-  fromCounter.counterFeatureKey
-);
