a34dd3f7f4563ce0110bd801fc6b2ef9c40d24e0
shawn <shawnrmoss@gmail.com>
2021-09-01 19:34:25 -0600

    Deleted

--- src/app/counter/counter-store.module.ts
+++ src/app/counter/counter-store.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
-import * as fromCounter from './store/counter.reducer';
 import { EffectsModule } from '@ngrx/effects';
-import { CounterEffects } from './store/counter.effects';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    StoreModule.forFeature(fromCounter.counterFeatureKey, fromCounter.reducer),
-    EffectsModule.forFeature([CounterEffects])],
+  ],
   providers: []
 })
 export class CounterStoreModule { }
